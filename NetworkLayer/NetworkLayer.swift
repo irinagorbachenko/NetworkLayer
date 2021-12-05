cf72c581dd4788f90e629020f6b9cab606109cf6
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2021-12-05 20:34:31 +0200

    tests added

---
+++ NetworkLayer/NetworkLayer.swift
@@ -0,0 +1,68 @@
+//
+//  NetworkLayer.swift
+//  NetworkLayer
+//
+//  Created by Irina Gorbachenko on 02.12.2021.
+//
+
+import Foundation
+
+import UIKit
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> ())
+}
+public protocol NetworkSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkTask
+}
+
+public protocol NetworkTask {
+    func resume()
+}
+
+public struct URLHTTPClient: HTTPClient {
+    public let session: NetworkSession
+    private struct UnexpectedArguments: Error {}
+    
+    public init(session: NetworkSession) {
+        self.session = session
+    }
+    
+    public func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> ()) {
+        let task: NetworkTask = session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else  if let error = error {
+                completion(.failure(error))
+            } else{
+                completion(.failure(UnexpectedArguments()))
+            }
+        }
+        task.resume()
+    }
+}
+
+
+
+public struct ImageLoader {
+    
+    public let session: HTTPClient
+    
+    public init(with session: HTTPClient) {
+        self.session = session
+    }
+    
+    public  func load(from url: URL, completion: @escaping (UIImage) -> ()) {
+        session.get(from: url) { (result) in
+            switch result {
+            case let .success(data, _):
+                completion(UIImage(data: data)!)
+            case let .failure(error):
+                break
+            }
+            
+        }
+    }
+}
+
+
--- NetworkLayerTests/NetworkLayerTests.swift
+++ NetworkLayerTests/NetworkLayerTests.swift
@@ -8,26 +8,115 @@
 import XCTest
 @testable import NetworkLayer
 
-class NetworkLayerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+protocol NetworkSession {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkTask
 }
+
+protocol NetworkTask {
+    func resume()
+}
+
+struct URLHTTPClient: HTTPClient {
+    let session: NetworkSession
+    private struct UnexpectedArguments: Error {}
+    
+    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> ()) {
+        let task: NetworkTask = session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            } else  if let error = error {
+                completion(.failure(error))
+            } else{
+                completion(.failure(UnexpectedArguments()))
+            }
+        }
+        task.resume()
+    }
+}
+
+
+class NetworkTaskSpy: NetworkTask {
+    var data :Data?
+    var response :URLResponse?
+    var error : Error?
+    
+    var isCalled = false
+    var completion: ((Data?, URLResponse?, Error?) -> Void)?
+
+    func resume() {
+        completion?(data, response, error)
+        isCalled = true
+    }
+}
+
+class NetworkSessionSpy: NetworkSession {
+
+    var taskSpy: NetworkTaskSpy = NetworkTaskSpy()
+
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> NetworkTask {
+        taskSpy.completion = completionHandler
+        return taskSpy
+    }
+}
+
+enum SomeError: Error { case some}
+
+class NetworkLayerTests: XCTestCase {
+   
+    func test_get_returnsErrorOnDataNilResponseNilErrorValid() {
+        let exp = expectation(description: "waiting for response")
+        let networkTask = NetworkTaskSpy()
+        let session = NetworkSessionSpy()
+        session.taskSpy = networkTask
+        networkTask.data = nil
+        networkTask.response = nil
+        networkTask.error = SomeError.some
+        let sut = URLHTTPClient(session: session)
+
+        let url = URL(string: "http://google.com")!
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(error):
+                XCTAssertEqual(SomeError.some, error as? SomeError)
+                exp.fulfill()
+            case let .success((data, response)):
+                XCTFail()
+            }
+        }
+
+        XCTAssertTrue(networkTask.isCalled)
+        wait(for: [exp], timeout: 0.1)
+    }
+        
+    
+    func test_get_returnsResponseDataErrorNilDataValid() {
+        let exp = expectation(description: "waiting for response")
+        let url = URL(string: "http://google.com")!
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        let data = Data()
+        let networkTask = NetworkTaskSpy()
+        let session = NetworkSessionSpy()
+        session.taskSpy = networkTask
+        networkTask.data = data
+        networkTask.response = response
+        networkTask.error = nil
+        let sut = URLHTTPClient(session: session)
+
+
+        sut.get(from: url) { result in
+            switch result {
+            case let .failure(error):
+                XCTFail()
+            case let .success((data, response)):
+                break
+            }
+            exp.fulfill()
+        }
+
+        XCTAssertTrue(networkTask.isCalled)
+        wait(for: [exp], timeout: 0.1)
+    }
+    
+}
+
